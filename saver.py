d9d2ae293137066faed73a905a86df6d0c4ccc38
Norbaeocystin <baranrastislav@gmail.com>
2019-02-14 14:56:51 +0100

    Update saver.py

--- saver.py
+++ saver.py
@@ -4,13 +4,14 @@ Short function using Pandas to export data from MongoDB to excel
 import pandas as pd
 from pymongo import MongoClient
 
-#connection to MongoDB
+# Connectio URI can be in shape mongodb://<username>:<password>@<ip>:<port>/<authenticationDatabase>')
 client = MongoClient('mongodb://localhost')
-
 
 def export_to_excel(name, collection, database):
     '''
     save collection from MongoDB as .xlsx file, name of file is argument of function 
+    collection <string> is name of collection 
+    database <string> is name of database
     '''
     data = list(client[database][collection].find({},{'_id':0}))
     df =  pd.DataFrame(data)
