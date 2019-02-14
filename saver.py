0d538dc76b46850683216ba976c5ebcc0cff918b
Norbaeocystin <baranrastislav@gmail.com>
2019-02-14 14:54:21 +0100

    Update saver.py

--- saver.py
+++ saver.py
@@ -12,7 +12,7 @@ def export_to_excel(name, collection, database):
     '''
     save collection from MongoDB as .xlsx file, name of file is argument of function 
     '''
-    data = list(client[databse][collection].find({},{'_id':0}))
+    data = list(client[database][collection].find({},{'_id':0}))
     df =  pd.DataFrame(data)
     #writer = pd.ExcelWriter('{}.xlsx'.format(name), engine='xlsxwriter')
     df.to_excel(writer, sheet_name='Sheet1')
